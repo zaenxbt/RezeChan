e56fc9f9e4395359b4b0b0af173ff69188e27905
zaenxbt <zaenxbt@gmail.com>
2025-11-27 02:25:58 +0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+return (
+<footer className="w-full py-12 text-center opacity-70">
+© {new Date().getFullYear()} Reze-Chan — The Privacy Native Meme Revolution
+</footer>
+)
+}
