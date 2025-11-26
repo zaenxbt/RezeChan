dd2959d839499c77f9760fcfa957fe5f91df0dfa
zaenxbt <zaenxbt@gmail.com>
2025-11-27 00:24:19 +0700

    Create page.tsx

---
+++ reze-chan landing/app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "../components/hero";
+import Features from "../components/features";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+
+export default function Page() {
+return (
+<main className="min-h-screen w-full overflow-hidden">
+<Header />
+<Hero />
+<Features />
+<Footer />
+</main>
+);
+}
+```
