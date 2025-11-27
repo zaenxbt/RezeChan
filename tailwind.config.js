a984f54ba6620e8738154ba2e6459f3625d91c34
zaenxbt <zaenxbt@gmail.com>
2025-11-27 10:37:46 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
 content: [
-'./app/**/*.{js,jsx}',
-'./components/**/*.{js,jsx}',
 './pages/**/*.{js,jsx}',
+'./components/**/*.{js,jsx}',
 ],
 theme: {
 extend: {
 colors: {
-rezeBlue: '#0b3d91',
-rezeMint: '#8ef7d6',
-},
-},
+rezeBlue: '#0b46e6',
+rezeDark: '#06132a'
+}
+}
 },
 plugins: [],
-};
+}
+```
